05bd4175573d680eb711c105e53f3beda427b76a
Victor Correa <victorac.public@gmail.com>
2018-12-06 15:39:32 -0800

    Adding documentation for the 'widgetImage' directive

--- app/scripts/directives/widgetImage/widgetImage.js
+++ app/scripts/directives/widgetImage/widgetImage.js
@@ -2,9 +2,15 @@
 
 /**
  * @ngdoc directive
+ *
  * @name uxAspectsDashing.directive:widgetImage
+ *
  * @description
- * # widgetImage
+ * Image (JPG, GIF, or PNG) available at a given URL.
+ *
+ * @element ANY
+ * @param {object} widgetImage Object in which the `'imageUrl'` property informs an accessible URL representing a .jpg, .gif, or .png image
+ * @param {string=} header Image title <br /> *(default: "")*
  */
 angular.module('uxAspectsDashing')
     .directive('widgetImage', function () {
