ef8d492686c030adafe917e155e963d07240f7b7
Victor Correa <victorac.public@gmail.com>
2018-12-06 16:56:30 -0800

    Adding documentation for the 'widgetProfile' directive

--- app/scripts/directives/widgetProfile/widgetProfile.js
+++ app/scripts/directives/widgetProfile/widgetProfile.js
@@ -2,9 +2,19 @@
 
 /**
  * @ngdoc directive
+ *
  * @name uxAspectsDashing.directive:widgetProfile
+ *
  * @description
- * # widgetProfile
+ * Displays information about an entity (e.g. address book contact, company, computer)
+ * which is represented by an image, a name and a list of key-value properties.
+ *
+ * @element ANY
+ * @param {object} widgetProfile Object with the following properties:
+ *                               * `'name'`: Name of the entity to be represented
+ *                               * `'picture'`: URL pointing to an image
+ *                               * `'value'`: Array of key-value pairs representing properties about the entity
+ * @param {string=} header Profile title <br /> *(default: "")*
  */
 angular.module('uxAspectsDashing')
     .directive('widgetProfile', function () {
