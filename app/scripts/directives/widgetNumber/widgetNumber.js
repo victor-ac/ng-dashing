c78efceb09573b4f55b3bc3c193be84b627d4e92
Victor Correa <victorac.public@gmail.com>
2018-12-06 16:42:50 -0800

    Adding documentation for the 'widgetNumber' directive

--- app/scripts/directives/widgetNumber/widgetNumber.js
+++ app/scripts/directives/widgetNumber/widgetNumber.js
@@ -2,9 +2,20 @@
 
 /**
  * @ngdoc directive
+ *
  * @name uxAspectsDashing.directive:widgetNumber
+ *
  * @description
- * # widgetNumber
+ * Displays a number and, optionally, the variation since the last value.
+ *
+ * @element ANY
+ * @param {object} widgetNumber Object in which the `'value'` property represents a number and
+ *                 `'variation'` represents the delta (in percentage) between the previous value and the current one
+ * @param {string=} header Number title <br /> *(default: "")*
+ * @param {string=} prefix String to be prepended to the number <br /> *(default: "")*
+ * @param {boolean=} showVariation Flag controlling whether or not the variation should be displayed <br />
+ *                                 *(default: false)*
+ * @param {string=} suffix String to be appended to the number <br /> *(default: "")*
  */
 angular.module('uxAspectsDashing')
     .directive('widgetNumber', function () {
@@ -13,10 +24,10 @@ angular.module('uxAspectsDashing')
             restrict: 'A',
             scope: {
                 'data': '=widgetNumber',
+                'header': '@',
                 'prefix': '@',
                 'showVariation': '=?',
-                'suffix': '@',
-                'header': '@'
+                'suffix': '@'
             },
             controller: function ($scope) {
                 if (_.isUndefined($scope.showVariation)) {
