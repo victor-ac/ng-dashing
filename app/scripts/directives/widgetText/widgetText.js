3f05a583ffe68f30627f225a3c56f6e5035985d2
Victor Correa <victorac.public@gmail.com>
2018-12-08 12:21:43 -0800

    Adding documentation for the 'widgetText' directive

--- app/scripts/directives/widgetText/widgetText.js
+++ app/scripts/directives/widgetText/widgetText.js
@@ -2,9 +2,18 @@
 
 /**
  * @ngdoc directive
+ *
  * @name uxAspectsDashing.directive:widgetText
+ *
  * @description
- * # widgetText
+ * Displays a text.
+ *
+ * @element ANY
+ * @param {object} widgetText Object with the following properties:
+ *                            * `'value'`: Text to be displayed
+ * @param {string=} header Text title <br /> *(default: "")*
+ * @param {boolean=} small Flag controlling whether or not to display the text in a smaller font
+ *                         <br /> *(default: false)*
  */
 angular.module('uxAspectsDashing')
     .directive('widgetText', function () {
